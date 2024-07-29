517bf8d836a5a1abd20f5c6eefe73d447b6ae3d3
hadeedtariq <computeranalog351@gmail.com>
2024-07-28 19:32:59 -0700

    make app a progressive web app

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,52 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Editex",
+    short_name:"editex",
+    description:"Editex a code ditor",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA(manifestForPlugIn as any)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
